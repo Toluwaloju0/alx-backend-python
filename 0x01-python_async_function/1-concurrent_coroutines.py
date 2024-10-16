304fd436cf0a5c1ae8d9c1ae223651b8467b401b
Toluwaloju0 <teecoded1999@gmail.com>
2024-10-16 19:57:05 +0100

    async

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -15,4 +15,6 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
 
     run_list = await asyncio.gather(*tasks)
 
-    return run_list
+    new_list: List[float] = sorted(run_list)
+
+    return new_list
