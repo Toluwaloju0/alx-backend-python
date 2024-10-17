b12457941cfb657eb97da1b064d5f3a7ed864dfe
Toluwaloju0 <teecoded1999@gmail.com>
2024-10-17 12:19:53 +0100

    async

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,11 +2,11 @@
 """A module to work on async comprehension"""
 
 import asyncio
-from typing import AsyncGenerator
+from typing import Generator
 from random import uniform
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]: # type: ignore
     """A function to create a generator number"""
 
     await asyncio.sleep(1)
