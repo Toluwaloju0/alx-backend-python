68535415c0c2e65b72c84e7bdaf0d06d24d65200
Toluwaloju0 <teecoded1999@gmail.com>
2024-11-05 05:07:20 +0100

    test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -9,6 +9,13 @@ from utils import access_nested_map
 
 class TestAccessNestedMap(unittest.TestCase):
     """A tet class for acceess_nested_map"""
+    def get_item(self, nested_map, path):
+        """A function to get the item at path"""
+
+        for key in path:
+            nested_map = nested_map.get(key)
+        return nested_map
+
     @parameterized.expand([
         ({"a": 1}, ("a")),
         ({"a": {"b": 2}}, ("a")),
@@ -16,4 +23,5 @@ class TestAccessNestedMap(unittest.TestCase):
         ])
     def test_access_nested_map(self, nested_map, path):
         """The test to make sure the function works correctly"""
-        self.assertEqual(access_nested_map(nested_map, path), nested_map.get(path))
+        self.assertEqual(access_nested_map(nested_map, path),
+                         self.get_item(nested_map, path))
