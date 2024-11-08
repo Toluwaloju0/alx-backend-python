14cd734962df2e02644e75007b089fc6e175833c
Toluwaloju0 <teecoded1999@gmail.com>
2024-11-08 21:49:14 +0100

    test clients

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -27,3 +27,21 @@ class TestGithubOrgClient(unittest.TestCase):
             mock_repos_url.return_value = "https://api.github.com/orgs/google/repos"
             new_class = GithubOrgClient('google')
             self.assertEqual(new_class._public_repos_url, "https://api.github.com/orgs/google/repos")
+
+    @patch('client.get_json')
+    def test_public_repos(self, mocked_get_json):
+        """A function to test the public_repos method"""
+        mock = Mock()
+        mock.json.return_value = ['strings']
+        mocked_get_json.return_value = mock
+        new_class = GithubOrgClient('google')
+        # patch to mock the public_repos_url"""
+        with patch(
+            'client.GithubOrgClient._public_repos_url',
+            new_callable=PropertyMock
+        ) as mock_repos_url:
+            mock_repos_url.return_value = "string"
+            self.assertEqual(new_class._public_repos_url, "string")
+            mock_repos_url.assert_called_once()
+        new_class.public_repos
+        # mocked_get_json.assert_called_once()
